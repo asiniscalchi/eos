b23bc3ae6b6dd07c9cb92be7471931008e18ecf9
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-05-14 10:39:18 +0200

    [sql_db_plugin] added mongoDB backend infrastructure

---
+++ plugins/sql_db_plugin/mongodb/irreversible_block_consumer.cpp
@@ -0,0 +1,10 @@
+#include "irreversible_block_consumer.h"
+
+namespace eosio {
+
+void irreversible_block_consumer::consume(const std::vector<chain::block_state_ptr> &blocks)
+{
+
+}
+
+} // namespace
---
+++ plugins/sql_db_plugin/mongodb/irreversible_block_consumer.h
@@ -0,0 +1,21 @@
+/**
+ *  @file
+ *  @copyright defined in eos/LICENSE.txt
+ */
+
+#pragma once
+
+#include "consumer_core.h"
+
+#include <eosio/chain/block_state.hpp>
+
+namespace eosio {
+
+class irreversible_block_consumer : public consumer_core<chain::block_state_ptr>
+{
+public:
+    void consume(const std::vector<chain::block_state_ptr>& blocks) override;
+};
+
+} // namespace
+
